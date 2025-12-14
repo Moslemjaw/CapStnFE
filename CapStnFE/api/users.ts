fc2d317c800fe45a00a872d8a874a3cecc8c424e
Musallam Jawish <Musallamjawish314@gmail.com>
2025-12-14 19:43:35 +0300

    setup

--- CapStnFE/api/users.ts
+++ CapStnFE/api/users.ts
@@ -6,20 +6,9 @@ const getUserById = async (userId: string): Promise<User> => {
   return data;
 };
 
-const getUserRecipes = async (userId: string): Promise<any[]> => {
-  // Get all recipes and filter by userId
-  const { data } = await instance.get("/recipes");
-  return data.filter((recipe: any) => {
-    const recipeUserId =
-      typeof recipe.userId === "object" ? recipe.userId._id : recipe.userId;
-    return recipeUserId === userId;
-  });
-};
-
 const getAllUsers = async (): Promise<User[]> => {
   const { data } = await instance.get("/users");
   return data;
 };
 
-export { getUserById, getUserRecipes, getAllUsers };
-
+export { getUserById, getAllUsers };
